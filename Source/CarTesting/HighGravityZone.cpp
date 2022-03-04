08665ed0daeb9477bc82df1cb99dfa2e99bb6bb0
Mathias M <mathmoe64@gmail.com>
2022-03-04 23:46:14 +0100

    awd

--- Source/CarTesting/HighGravityZone.cpp
+++ Source/CarTesting/HighGravityZone.cpp
@@ -10,6 +10,8 @@ AHighGravityZone::AHighGravityZone()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 	BoxComp = CreateDefaultSubobject<UBoxComponent>(TEXT("Box Component"));
+	
+	
 }
 
 // Called when the game starts or when spawned
