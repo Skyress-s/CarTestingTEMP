c124c70116b9224092830ab57b7d5a4068eb3e4b
Anders Petershagen Åsbø <39097620+FunkMarvel@users.noreply.github.com>
2022-03-30 12:25:29 +0200

    Added drone class

---
+++ Source/CarTesting/Enemies/DroneActor.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseEnemyActor.h"
+#include "DroneActor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CARTESTING_API ADroneActor : public ABaseEnemyActor
+{
+	GENERATED_BODY()
+	
+};
