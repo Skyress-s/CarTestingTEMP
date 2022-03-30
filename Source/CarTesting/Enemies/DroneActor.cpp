10375dbb0f0231cf6022f978ee9f09c6e63460ed
Mathias M <mathmoe64@gmail.com>
2022-03-30 13:37:10 +0200

    sync

---
+++ Source/CarTesting/Enemies/DroneActor.cpp
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DroneActor.h"
+
+ADroneActor::ADroneActor()
+{
+}
+
+void ADroneActor::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ADroneActor::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+}
---
+++ Source/CarTesting/Enemies/DroneActor.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseEnemyActor.h"
+#include "DroneActor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CARTESTING_API ADroneActor : public ABaseEnemyActor
+{
+	GENERATED_BODY()
+
+public:
+	ADroneActor();
+
+	void BeginPlay() override;
+	void Tick(float DeltaSeconds) override;
+};
