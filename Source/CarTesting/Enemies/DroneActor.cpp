dba81336848a7909b4e92873043631d58b6a3b4a
Anders Petershagen Åsbø <39097620+FunkMarvel@users.noreply.github.com>
2022-04-12 13:27:57 +0200

    Added drone

--- Source/CarTesting/Enemies/DroneActor.cpp
+++ Source/CarTesting/Enemies/DroneActor.cpp
@@ -3,6 +3,10 @@
 
 #include "DroneActor.h"
 
+#include "CarTesting/CarPawn.h"
+#include "CarTesting/GravitySplineActor.h"
+#include "Components/SphereComponent.h"
+
 ADroneActor::ADroneActor()
 {
 }
@@ -15,4 +19,60 @@ void ADroneActor::BeginPlay()
 void ADroneActor::Tick(float DeltaSeconds)
 {
 	Super::Tick(DeltaSeconds);
+
+	switch (CurrentState)
+	{
+	case EDroneState::Intercepting:
+		InterceptingState();
+		break;
+	case EDroneState::Attacking:
+		AttackingState();
+		break;
+	case EDroneState::Idle:
+		if (PlayerPawn && GravitySplineActive) ChangeState(Intercepting);
+		break;
+	}
+	if (!PlayerPawn)
+	{
+		PlayerPawn = Cast<ACarPawn>(GetWorld()->GetFirstPlayerController()->GetPawn());
+	}
+	else if (!GravitySplineActive)
+    {
+    	GravitySplineActive = PlayerPawn->GravitySplineActive;
+    }
+}
+
+void ADroneActor::InterceptingState()
+{
+	if (bEnteringState)
+	{
+		InterceptSpeed = (TargetLocation-GetActorLocation()).Size()/InterceptTime;
+		TargetLocation = PlayerPawn->GetActorLocation() + PlayerPawn->GetActorForwardVector()*ForwardOffset;
+		bEnteringState = false;
+		
+	}
+	TargetLocation.Z = GetActorLocation().Z;
+	Move(TargetLocation);
+	if (GetActorLocation().Equals(TargetLocation))
+	{
+		ChangeState(EDroneState::Attacking);
+	}
+}
+
+void ADroneActor::AttackingState()
+{
+	if (bEnteringState)
+	{
+		bEnteringState = false;
+	}
+}
+
+void ADroneActor::Move(FVector Target)
+{
+	InterceptTimer += GetWorld()->GetDeltaSeconds();
+	SphereComp->AddRelativeLocation((TargetLocation-GetActorLocation()).GetSafeNormal()*InterceptSpeed*InterceptTimer);
+	if (InterceptTimer >= InterceptTime)
+	{
+		ChangeState(EDroneState::Attacking);
+	}
 }
--- Source/CarTesting/Enemies/DroneActor.h
+++ Source/CarTesting/Enemies/DroneActor.h
@@ -17,6 +17,37 @@ class CARTESTING_API ADroneActor : public ABaseEnemyActor
 public:
 	ADroneActor();
 
-	void BeginPlay() override;
-	void Tick(float DeltaSeconds) override;
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaSeconds) override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Movement")
+		float ForwardOffset{500.f};
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Movement")
+		float InterceptTime{3.f};
+	float InterceptTimer{};
+	float InterceptSpeed{};
+	
+protected:
+	FVector TargetLocation{};
+
+	enum EDroneState
+	{
+		Idle,
+		Intercepting,
+		Attacking
+	};
+
+	EDroneState CurrentState{EDroneState::Idle};
+	bool bEnteringState{false};
+
+	void ChangeState(EDroneState NewState)
+	{
+		bEnteringState = true;
+		CurrentState = NewState;
+	}
+
+	void InterceptingState();
+	void AttackingState();
+
+	void Move(FVector Target);
 };
